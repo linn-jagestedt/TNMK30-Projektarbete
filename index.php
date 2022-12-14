833b45201a9438075d724cb7140d633b28b9b850
noelrosen <117373405+noelrosen@users.noreply.github.com>
2022-12-14 11:31:29 +0100

    index sida klar

--- index.php
+++ index.php
@@ -12,18 +12,17 @@
     <body>
         <main class="index_main">
             <div class='bakgrund'>
-                <img src='https://t3.ftcdn.net/jpg/02/66/56/76/360_F_266567677_5ZcfjBF5nsk6UwJqhhmwuZYIB6io1p1w.webp' alt="lego">
+                <img src='./images/lego2.png' alt="lego">
             </div>
             <div><h1 class='titel'>Legobase</h1></div>
             <div class='sokfalt'>
                 <form action="searchResult.php" method="get">
-                    <label calss='sok-text'><p>Search</p></label>
+                    <label class='sok-text'><p>Search</p></label>
                     <input type="text" name="searchTerm" class='sok' placeholder=" Find a brick...">
                 </form>
             </div>
             <div class="push"></div>
         </main>
+        <?php include("footer.php"); ?>
     </body>
-
-    <?php include("footer.php"); ?>
 </html>  
